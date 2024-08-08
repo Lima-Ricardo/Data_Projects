3ec0f47134492645a74302860ee3ca7b24ac7767
Lima-Ricardo <ricardoalexdel@gmail.com>
2024-08-08 14:02:29 -0300

    codigo refatorado

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ print(soma)
 #Desafio do dia: Cálculo de Bônus com Entrada do Usuário
 #Escreva um programa em Python que solicita ao usuário para digitar seu nome, o valor do seu salário mensal e o valor do bônus que recebeu. O programa deve, então, imprimir uma mensagem saudando o usuário pelo nome e informando o valor do salário em comparação com o bônus recebido.
 
-nome = input('Digite o seu texto: ')
+nome = str(input('Digite o seu texto: '))
 salario = nome = int(input('Digite o seu salario: '))
-percentual = 1.5
-bonus = 1000 + salario * percentual
+PERCENTUAL = 1.5
+bonus = 1000 + salario * PERCENTUAL
 print(f"Meu nome é: {nome}, e meu bonus foi de: {bonus}")
